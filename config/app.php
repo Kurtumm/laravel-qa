5031eadd1133002cdd0d5fd0585a5066f402b2b1
Nattawoot Peanpunyaruk <kurtumm@gmail.com>
2019-08-15 17:35:49 +0700

    Display all questions

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Laravel QA'),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:Tsiur9pU+5Cc4vgqIlkGrFmPXVDsFIUJjruB9N/OB8c='),
 
     'cipher' => 'AES-256-CBC',
 
